c837b5a68c079be44cc615ce2ccb0ea7886e4329
Susana Fuentes <susana.fuentes@rivm.nl>
2017-05-10 13:04:48 +0000

    new modifications

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -26,3 +26,10 @@ rm Trainset14_032015.pds.tgz
 # Generate a customized version of the SILVA reference database that targets the V4 region
 code/mothur/mothur "#pcr.seqs(fasta=data/references/silva.seed.align, start=11894, end=25319, keepdots=F, processors=8)"
 mv data/references/silva.seed.pcr.align data/references/silva.v4.align
+
+# Run mothur through the various quality control steps
+code/mothur/mothur code/get_good_seqs.batch
+
+# Run mock community data through mothur to calculate the sequencing error rates
+code/mothur/mothur code/get_error.batch
+
